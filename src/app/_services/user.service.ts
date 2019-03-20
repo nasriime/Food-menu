e9d80357a98f5a0ea298b3f70cba9193aacbb741
nasriime <mnasr4040@gmail.com>
2019-03-20 06:54:53 +0200

    push app

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '@/_models';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${config.apiUrl}/users`);
+    }
+
+    getById(id: number) {
+        return this.http.get<User>(`${config.apiUrl}/users/${id}`);
+    }
+
+    public getMenu(): Observable<any> {
+        return this.http.get("/menu.json");
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@/_models';
+import { UserService, AuthenticationService } from '@/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    currentUser: User;
+    userFromApi: User;
+    query: string;
+    appetizers: [];
+    filteredAppetizers: any[]= [];
+    salads: [];
+    filteredSalads: any[]= [];
+
+    constructor(
+        private userService: UserService,
+        private authenticationService: AuthenticationService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.userService.getById(this.currentUser.id).pipe(first()).subscribe(user => {
+            this.userFromApi = user;
+        });
+        this.userService.getMenu().subscribe(items => {
+            this.appetizers = items.categories[0].items;
+            this.salads = items.categories[1].items;
+            this.assignCopy();
+        });
+    }
+
+    assignCopy(){
+        this.filteredAppetizers = Object.assign([], this.appetizers);
+        this.filteredSalads = Object.assign([], this.salads);
+     }
+
+     filterItem(){
+        if(!this.query){
+            this.assignCopy();
+        }
+        this.filteredAppetizers = Object.assign([], this.appetizers).filter(
+           item => item.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1
+        )
+        this.filteredSalads = Object.assign([], this.salads).filter(
+            item => item.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1
+         )
+     }
+
+}
